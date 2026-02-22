f22f6bb0af49978f37d7babab6499377bd0ebfa3
tima391 <timka0427@gmail.com>
2026-02-22 18:57:45 +0500

    Update Main5.java

--- lab5H/Main5.java
+++ lab5H/Main5.java
@@ -2,18 +2,15 @@ package lab5H;
 
 public class Main5 {
     public static void main(String[] args) {
-        // 1. Singleton тест
         ConfigurationManager config = ConfigurationManager.getInstance();
         config.setSetting("Language", "RU");
         config.saveToFile();
 
-        // 2. Builder тест
         ReportDirector director = new ReportDirector();
         HtmlReportBuilder htmlBuilder = new HtmlReportBuilder();
         director.construct(htmlBuilder);
         System.out.println(htmlBuilder.getReport());
 
-        // 3. Prototype тест
         Order order1 = new Order();
         order1.products.add(new Product("Молоко", 100));
         order1.payment = "Карта";
@@ -24,4 +21,5 @@ public class Main5 {
         System.out.println("Оригинал оплата: " + order1.payment);
         System.out.println("Клон оплата: " + order2.payment);
     }
+
 }
